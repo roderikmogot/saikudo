5a2cd205154d5ffaf46e7c11cc59058ce6c4ec56
roderikmogot <roderikmogot17@gmail.com>
2022-07-16 15:46:18 +0800

    cleaning admin pages and admin navbar

--- src/components/AdminNavBar.jsx
+++ src/components/AdminNavBar.jsx
@@ -1,4 +1,4 @@
-import "../css/Edit.css";
+import "../css/History.css";
 import Logo from "../img/logo.png";
 import { Link } from "react-router-dom";
 
@@ -7,33 +7,34 @@ const AdminNavBar = () => {
     <div className="admin-navigation-bar">
       <img className="admin-navigation-logo" src={Logo} alt="Logo" />
       <ul className="admin-navigation">
-        <li className="admin-navigation-menu">
+        <li className="admin-navigation-li">
           <Link
+            className="admin-navigation-menu"
             to="/admin"
-            style={{ textDecoration: "none", color: "#745656" }}
           >
             Unpaid
           </Link>
         </li>
-
-        <li className="admin-navigation-menu">
-          <Link to="/paid" style={{ textDecoration: "none", color: "#745656" }}>
+        <li className="admin-navigation-li">
+          <Link
+            className="admin-navigation-menu"
+            to="/paid"
+          >
             Paid
           </Link>
         </li>
-        <li className="admin-navigation-menu">
+        <li className="admin-navigation-li">
           <Link
+            className="admin-navigation-menu"
             to="/history"
-            style={{ textDecoration: "none", color: "#745656" }}
           >
             Riwayat Pesanan
           </Link>
         </li>
-        <li className="admin-navigation-menu">
+        <li className="admin-navigation-li">
           <Link
             to="/edit"
-            className="admin-navigation-link"
-            style={{ textDecoration: "none", color: "#745656" }}
+            className="admin-navigation-menu"
           >
             Ubah Menu
           </Link>
